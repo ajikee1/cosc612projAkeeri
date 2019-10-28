f83c7bd5d963b41e816679abd64a20bc79eb36c3
ajikee1 <45089138+ajikee1@users.noreply.github.com>
2019-10-28 14:45:22 -0400

    Add files via upload
    Use this page to load products and images to the database

---
+++ productUpload.php
@@ -0,0 +1,59 @@
+<?php
+
+include 'dbConnection.php';
+
+$status = uploadProduct($servername, $username,$password, $db);
+echo $status;
+
+//CREATE TABLE `caffeineDB`.`products` ( `productId` INT NOT NULL AUTO_INCREMENT , `productName` VARCHAR(50) NOT NULL , `productPrice` DOUBLE(5,2) NOT NULL , `stock` INT(5) NOT NULL , `productDescription` VARCHAR(200) NOT NULL , `productImage` VARCHAR(300) NOT NULL , PRIMARY KEY (`productId`(5))) ENGINE = InnoDB;
+function uploadProduct($servername, $username,$password, $db)
+{
+    $dbConnection = mysqli_connect($servername, $username, $password, $db);
+
+    // If the upload button is clicked
+    if (isset($_POST['uploadProducttoDB'])) {
+         $productImage = $_FILES['productImage']['name'];
+        $productName = $_POST['productName'];
+        $productPrice = $_POST['productPrice'];
+        $productStock = $_POST['stock'];
+        $productDescription = $_POST['productDescription'];
+
+        // target directory where the product image will be stored into
+        $target = "images/".basename($productImage);
+
+        $query = "INSERT INTO products (productName, productPrice, stock, productDescription, productImage) VALUES ('$productName', '$productPrice', '$productStock','$productDescription', '$productImage')";
+
+
+        mysqli_query($dbConnection, $query);
+
+        if (move_uploaded_file($_FILES['productImage']['tmp_name'], $target)) {
+            $msg = "Image uploaded successfully";
+        }else{
+            $msg = "Failed to upload image";
+        }
+        return $msg;
+    }
+}
+?>
+
+<html>
+    <head>
+        <title>
+            Supplier app product portal
+        </title>
+    </head>
+    <body>
+
+        <h1>Supplier Product Restock portal</h1>
+        <h3>Suppliers can use this portal to add products to CaffeineIOT</h3>
+        <form method="post" action="productUpload.php" enctype="multipart/form-data">
+            Product image: <input type="file" name="productImage">
+            Product name: <input type="text" name="productName">
+            Product price: <input type="number" name="productPrice">
+            Product stock: <input type="number" name="stock">
+            Product description: <textarea name="productDescription" rows="10" cols="10" placeholder="Enter product details here..."></textarea>
+            <button type="submit" name="uploadProducttoDB">Add product</button>
+
+        </form>
+    </body>
+</html>
