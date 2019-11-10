a10ce13376e2dcecb4f6cd13c398ec370c80eac4
ajikee1 <Ajith.Keerikkattil@mantech.com>
2019-11-09 20:26:43 -0500

    paypal integration

--- customerCart.php
+++ customerCart.php
@@ -5,6 +5,7 @@ include 'dbConnection.php';
 
     getAllProductsInCart($servername, $username, $password, $db);
 
+
     function getAllProductsInCart($servername, $username, $password, $db)
         {
 
@@ -31,9 +32,9 @@ include 'dbConnection.php';
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
         <style>
-            h2, table{
+            h2, table, h3{
                 font-family: Bungee;
-                font-size: large;
+                font-size: x-large;
             }
 
             #total{
@@ -43,6 +44,20 @@ include 'dbConnection.php';
 
             button {
                 font-size: large;
+                font-family: Bungee;
+            }
+
+            #displayShippingAddress{
+                padding-left: 25%;
+                font-size: large;
+            }
+
+            #purchaseButtonDIV{
+                padding-left: 65%;
+            }
+
+            #addShippingAddress{
+                font-size: xx-large;
             }
         </style>
 
@@ -164,6 +179,8 @@ include 'dbConnection.php';
                     }
 
                     document.getElementById("total").innerHTML = "TOTAL AMOUNT DUE: $ " + total.toString();
+
+                    document.getElementById("payPalButton").elements[4].value = total;
                 }
 
             </script>
@@ -171,6 +188,96 @@ include 'dbConnection.php';
             <!--------------------------End Get the total due ---------------------------------------------------------------------->
 </div>
 
+        <?php
+
+        function getShippingAddress($servername, $username, $password, $db)
+        {
+
+            $shippingStreet = null; $shippingCounty= null; $shippingState= null; $shippingZip= null;
+            $dbConnection = mysqli_connect($servername, $username, $password, $db);
+
+            $customerEmail = $_SESSION["email"];
+
+            $query = "SELECT * FROM customerDetails WHERE email = '$customerEmail'";
+
+            $result = mysqli_query($dbConnection, $query);
+
+            if (mysqli_num_rows($result) >= 0) {
+                while ($row = mysqli_fetch_array($result)) {
+                    $shippingStreet = $row['street'];
+                    $shippingCounty = $row['county'];
+                    $shippingState = $row['state'];
+                    $shippingZip = $row['zip'];
+                }
+            }
+        ?>
+        <div>
+            <div id="ShippingAddress">
+                    <h3 align="center">Shipping Address</h3>
+                    <form id="displayShippingAddress">
+                        <div class="form-group w-50">
+                            <label for="Street">Street Name:</label>
+                            <input class="form-control" type="text" name="Street" value="<?php echo $shippingStreet; ?>"> <br>
+                        </div>
+
+                        <div class="form-group w-25">
+                            <label for="County">County:</label>
+                            <input class="form-control" type="text" name="County" value="<?php echo $shippingCounty; ?>" > <br>
+                        </div>
+
+                        <div class="form-group w-25">
+                            <label for="State">State:</label>
+                            <input class="form-control" type="text" name="State" value="<?php echo $shippingState; ?>" > <br>
+                        </div>
+
+                        <div class="form-group w-25">
+                            <label for="Zip">Zip Code:</label>
+                            <input class="form-control" type="text" name="Zip" value="<?php echo $shippingZip; ?>" > <br>
+                        </div>
+                </form>
+            </div>
+        </div>
+
+        <!-----------------------------------------------------PayPal Button------------------------------------------------------------->
+        <div id ="purchaseButtonDIV">
+            <div class = "paypal" id ="paypal">
+                <!--Paypal Button -->
+                <form target="_self" id="payPalButton" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_xclick">
+                    <input type="hidden" name="business" value="ajikee1@yopmail.com">
+                    <input type="hidden" name="lc" value="US">
+                    <input type="hidden" name="item_name" value="smartDevicePurchase">
+                    <input type="hidden" name="amount" value="">
+                    <input type="hidden" name="currency_code" value="USD">
+                    <input type="hidden" name="button_subtype" value="services">
+                    <input type="hidden" name="no_note" value="0">
+                    <input type="hidden" name="cn" value="Add special instructions to the seller:">
+                    <input type="hidden" name="no_shipping" value="2">
+                    <input type="hidden" name="custom" id="custom" value=" ">
+                    <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
+                    <input type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png" alt="Check out with PayPal" />
+                    <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                </form>
+
+                <script>
+                  //  var amnt = document.getElementById("payPalButton").elements[4].value;
+
+                  //  document.getElementById("test").innerHTML = amnt;
+                </script>
+                <p> Note: You will be redirected to the PayPal © website for payment processing. <br/>Once complete, you will be redirected back to Caffeine </p>
+            </div>
+        </div>
+
+            <!-----------------------------------------------------End PayPal Button------------------------------------------------------------->
+
+            <?php
+        mysqli_free_result($result);
+        mysqli_close($dbConnection);
+        }
+
+        getShippingAddress($servername, $username, $password, $db);
+
+        ?>
 
     </body>
 </html>
